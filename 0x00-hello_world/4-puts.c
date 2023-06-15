d310fe1bdb05a695f8b00e88430c752fc278128a
MaKairos <chriseldasihle94@gmail.com>
2023-06-15 05:40:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - C program that prints a statement using puts
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 Return(0)
 }
