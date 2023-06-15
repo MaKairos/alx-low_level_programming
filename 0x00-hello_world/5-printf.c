21ea2127e15361d8058e6b7385fd6f4e54cb1d75
MaKairos <chriseldasihle94@gmail.com>
2023-06-15 15:49:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#includ<stdio.h>
 /**
- * Entry point -  C program that prints a line Using the function printf
+ * Entry point - C program that prints a line Using the function printf
  * Return: 0 (success)
  */
-
 int Entry point(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
