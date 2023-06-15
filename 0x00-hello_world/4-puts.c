58ac1e0c65036145017699d7937d0324aecce4d8
MaKairos <chriseldasihle94@gmail.com>
2023-06-15 05:29:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Entry point - C program that prints a statement using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int Entry point(void)
+{
+puts("Programming is like building a multilingual puzzle");
+Return(0)
+}
