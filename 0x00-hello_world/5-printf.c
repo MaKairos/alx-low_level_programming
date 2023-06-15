afd80e6d48a32f4d71a6d725cf2fa53462e62de4
MaKairos <chriseldasihle94@gmail.com>
2023-06-15 14:54:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Entry point -  C program that prints a line Using the function printf
+ * Return: 0 (success)
+ */
+
+int Entry point(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
