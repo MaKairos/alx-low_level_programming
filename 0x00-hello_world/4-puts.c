d15e658439d0ef3cbe3946ab4901cd57883b4566
MaKairos <chriseldasihle94@gmail.com>
2023-06-15 05:37:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Entry point - C program that prints a statement using puts
+ * main - C program that prints a statement using puts
  *
  * Return: Always 0 (Success)
  */
-int Entry point(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 Return(0)
