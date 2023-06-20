2ca5e60e06a0a44d38eee79b2db18819b1222505
MaKairos <chriseldasihle94@gmail.com>
2023-06-20 14:25:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,20 +10,20 @@ int print_sign(int n)
 
 {
 
-if (n > 0)
-{
-_putchar(73);
-return (1);
-}
-else if (n < 0)
-{
-_putchar(45);
-return (-1);
-}
-else
-{
-_putchar(48);
-return (0);
-}
+	if (n > 0)
+	{
+	_putchar(73);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
 
 }
